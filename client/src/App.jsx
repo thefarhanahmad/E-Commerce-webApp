27384e0b2184fd6dfba8dc6bbd3c26838aa120c5
farhan <akhtarfarhan281@gmail.com>
2024-03-19 10:32:05 +0530

    working on frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,32 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
-import Search from "./pages/Search";
-import Cart from "./pages/Cart";
-import AdminDashboard from "./pages/AdminDashboard";
+import Navbar from "./components/Navbar";
+import Loader from "./components/Loader";
+import { Suspense, lazy } from "react";
+import Orders from "./pages/Orders";
+
+// importing pages
+const Home = lazy(() => import("./pages/Home"));
+const Search = lazy(() => import("./pages/Search"));
+const Cart = lazy(() => import("./pages/Cart"));
+const AdminDashboard = lazy(() => import("./pages/AdminDashboard"));
 
 function App() {
   return (
-    <div className="text-center mt-64 bg-red-500">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/admin/dashboard-admin" element={<AdminDashboard />} />
-      </Routes>
+    <div className="text-center bg-red-500">
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Routes */}
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<Orders />} />
+          <Route path="/admin/dashboard-admin" element={<AdminDashboard />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,80 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { GoSearch } from "react-icons/go";
+import { BsBag } from "react-icons/bs";
+import { AiOutlineUser } from "react-icons/ai";
+import { CiLogin, CiLogout } from "react-icons/ci";
 const Navbar = () => {
-  return (
-    <div>
-      navbar
-    </div>
-  )
-}
+  // fetching user
+  const user = {
+    name: "user",
+    role: "Admin",
+  };
 
-export default Navbar
+  // state var to control dialog modal
+  const [dialog, setDialog] = useState(false);
+  console.log("dialog : ", dialog);
+
+  const logOutHandler = () => {
+    setDialog(false);
+  };
+
+  return (
+    <div className="bg-green-400 flex gap-4 h-16 justify-between px-6 items-center">
+      {/* icon */}
+      <span>ShoppingCart</span>
+
+      {/* navlinks */}
+      <div className="flex gap-4 justify-center items-center">
+        <Link onClick={() => setDialog(false)} to={"/"}>
+          Home
+        </Link>
+        <Link onClick={() => setDialog(false)} to={"/search"}>
+          <GoSearch />
+        </Link>
+        <Link onClick={() => setDialog(false)} to={"/cart"}>
+          <BsBag />
+        </Link>
+
+        {/* if user exists */}
+        <div className="relative">
+          {user ? (
+            <button onClick={() => setDialog((pre) => !pre)}>
+              <AiOutlineUser />
+            </button>
+          ) : (
+            <span>
+              <CiLogin />
+            </span>
+          )}
+
+          {/* dialog */}
+          <dialog
+            open={dialog}
+            className="w-fit absolute p-2 top-8 -left-16 text-sm rounded-sm border "
+          >
+            <div className=" w-full flex flex-col gap-1 justify-center items-center">
+              {user.role === "Admin" && (
+                <Link
+                  className=""
+                  onClick={() => setDialog(false)}
+                  to={"admin/dashboard-admin"}
+                >
+                  Dashboard
+                </Link>
+              )}
+              <Link onClick={() => setDialog(false)} to={"/order"}>
+                Orders
+              </Link>
+              <button onClick={logOutHandler} to={"/"}>
+                <CiLogout />
+              </button>
+            </div>
+          </dialog>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
